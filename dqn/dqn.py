28e061f675bccd76e16500d9acd8b72dd58094d6
Nupomaru <atomic1017@gmail.com>
2020-02-11 16:49:49 +0900

    s

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -8,10 +8,14 @@ from keras.optimizers import Adam
 
 class DQN_Solver:
     def __init__(self, state_size, action_size):
-        self.input_shape = (12, 1)
+        self.input_shape = (12, 5)
         self.action_shape = (2,)
 
-        self.volume  = 1
+        self.priod = 1000 # bars
+
+
+        self.ts = trade_system('Bitmex1h')
+        self.found  = 10000 #dallrs
 
         self.action = [0, 1, 0]
         self.position_value = None
@@ -32,57 +36,82 @@ class DQN_Solver:
 
         def train(self):
             episodes = 100
-            ts = 100
             for e in range(episodes):
+                start_time = np.random.randint(0, len(self.ts.data) * 0.8 - (self.priod + self.input_shape[0]))
+                for t in range(self.priod):
+                    self.ts.time = start_time + t + self.input_shape[0]
 
-                for t in range(ts):
-                    self.action = self.choose_action()
-                    reword, done = self.reword()
+                    action = self.choose_action()
+                    reword, done = self.reword(action)
 
                     if done:
                         break
 
 
         # choosing action depending on epsilon
-        def choose_action(self, state):
+        def choose_action(self):
             if self.epsilon >= np.random.random():
                 # randomly choosing action
-                n = np.random.random()
-                return [n, 1-n]
+                return np.random.randint(0,3)
             else:
                 # choosing the best action from model.predict()
-                return self.choose_best_action(state)
+                return self.choose_best_action()
 
         def choose_best_action(self, state):
-            inp = 0
-            self.action = self.model.predict([inp,self.action])
+            inp1 = self.ts.data[self.ts.time - self.input_shape[0]:self.ts.time]
+            if self.ts.position['dir'] == 'LONG':
+                inp2 = [1,0,0]
+            elif self.ts.position['dir'] == None:
+                inp2 = [0,1,0]
+            elif self.ts.position['dir'] == 'SHORT':
+                inp2 = [0,0,1]
+            action = self.model.predict([inp1,inp2])
 
             return
 
-        def reword(self):
-            pass
+        def reword(self,action):
+            if action == 0:
+                ts.order_quantity('LONG',self.found)
+            elif action == 2:
+                ts.order_quantity('SHORT', self.found)
+            prof = self.ts.profit + self.ts.calc_inprofit()
+            if self.found - prof < self.found * 0.7: #loscut
+                return -100, False
+            else
+                return  prof, True
 
 class trade_system:
-    def __init__(self):
+    def __init__(self,name):
         self.position = {'dir':None,'price':None,'volume':None}
         self.time = 0
         self.spread = 0.03
         self.profit = 0
+        self.inprofit = 0
+
+        self.data = np.loadtxt('../data/%s.csv'%name, delimiter=',',skiprows=1,usecols=[2,3,4,5,6])
+        self.time = 0
+
+    def updata_time(self):
+        self.time += 1
 
     def get_price(self,dir):
-        res = 100
+        res = self.data[self.time,3]
         if dir == 'LONG':
             res *= (1 + self.spread/100)
         elif dir == 'SHORT':
             res *= (1 - self.spread/100)
         return res
 
+    def order_quantity(self,dir = None,quantity = 0):
+        if dir != None:
+            price = self.get_price(dir)
+            volume = quantity/price
+            self.upgrade_poition(dir, price, volume)
 
-
-    def order(self,dir,volume):
-        price = self.get_price(dir)
-
-        self.upgrade_poition(dir, price, volume)
+    def order(self,dir = None,volume = 0):
+        if dir != None:
+            price = self.get_price(dir)
+            self.upgrade_poition(dir, price, volume)
 
 
     def upgrade_poition(self, dir, price, volume):
@@ -114,16 +143,16 @@ class trade_system:
         elif self.position['dir'] == 'SHORT':  # 売ポジの場合
             self.profit -= (price - self.position['price']) * volume
 
-    def calc_inprofit(self, price):
+    def calc_inprofit(self):
         if self.position['dir'] == 'SHORT': # 売りポジの場合
-            return -(price - self.position['price']) * self.position['volume']
+            return -(self.get_price('LONG') - self.position['price']) * self.position['volume']
         elif self.position['dir'] == 'LONG':  # 買いポジの場合
-            return  (price - self.position['price']) * self.position['volume']
+            return  (self.get_price('SHORT') - self.position['price']) * self.position['volume']
         else:
             return 0
 
 
 
 if __name__ == '__main__':
-    ts = trade_system()
+    ts = trade_system('Bitmex1h')
     print()
